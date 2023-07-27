352c6aca85fc25177ae8ffaf8d4d28c60c944668
Hung Thinh <hungthinhh2003@gmail.com>
2023-07-27 15:38:32 +0700

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/views/nav.css 	new file:   src/views/nav.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/nav.js';
 
 function App() {
+  const link = 'https://www.w3schools.com/howto/howto_js_topnav.asp';
+
   return (
     <div className="App">
+      <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world</h1>
+        <a href={link}> Đi thôi</a> 
       </header>
     </div>
   );
