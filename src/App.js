c0557cda39fe010af52040001052fd1df1fcfea0
Hung Thinh <hungthinhh2003@gmail.com>
2023-07-28 15:59:26 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/views/nav.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/nav.js';
-
+import React, { useState } from 'react';
 function App() {
   const link = 'https://www.w3schools.com/howto/howto_js_topnav.asp';
+  
+  let [name, setName] = useState('HT')
+  let [address, setAddress] = useState('')
 
+  const clickMe = (event) => {
+    if(!address){
+      alert('emtpy input')
+      return;
+    }
+    
+    const newTodo = {id:'aa', title: address}
+    setTodos([...todos, newTodo])
+    setAddress('')
+  }
+  
+  const handleOnChange = (event) => {
+    setAddress(event.target.value)
+  }
+
+  let [todos, setTodos] = useState([
+    {id: 'todo1', title: 'siuuuu'},
+    {id: 'todo2', title: 'gooooo'},
+  ])
   return (
     <div className="App">
       <Nav/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world</h1>
+        <img style={{marginTop : '-100px'}} src={logo} className="App-logo" alt="logo" />
+        <h1>Hello {name}</h1>
         <a href={link}> Đi thôi</a> 
+        <div className='todos-container'>
+          {todos.map(todo => {
+            return(
+              <li className='todos-child' key={todo.id}> {todo.title}</li>
+            )
+          })}
+         
+        </div>
+        <input type='text' value={address} onChange = {(event) =>handleOnChange(event)}/> 
+        <button onClick = {(event) =>clickMe(event)}>Click me</button>
       </header>
     </div>
   );
--- src/views/nav.js
+++ src/views/nav.js
@@ -1,9 +1,9 @@
-import './nav.css'
+import './nav.scss'
 
 const Nav = () =>{
     return(
-        <div class="topnav">
-            <a class="active" href="#home">Home</a>
+        <div className="topnav">
+            <a className="active" href="#home">Home</a>
             <a href="#news">News</a>
             <a href="#contact">Contact</a>
             <a href="#about">About</a>
