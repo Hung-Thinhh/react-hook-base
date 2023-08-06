dd07373fca6e5c6fbefa43c2bbd9df5d04615a0e
Hung Thinh <hungthinhh2003@gmail.com>
2023-08-06 16:07:15 +0700

    new file:   src/customize/fetch.js 	modified:   src/views/covid.js

---
+++ src/customize/fetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (url) => {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
+
+    useEffect(() => {
+        try {
+
+
+            // let data = getData() && getData().data ? getData().data : [];
+            async function fetchData() {
+
+                let res = await axios.get(url);
+                let data = (res && res.data) ? res.data : [];
+
+                setData(data);
+                setLoading(false);
+                setIsError(false);
+                console.log(data[0])
+
+            }
+
+            // Gọi hàm fetchData() để lấy dữ liệu từ API
+            fetchData();
+        } catch (e) {
+            setLoading(false)
+            setIsError(true)
+        }
+
+    }, []);
+    return {data, loading, isError}
+}
+export default useFetch;
--- src/views/covid.js
+++ src/views/covid.js
@@ -1,63 +1,50 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import useFetch from '../customize/fetch.js';
 
 const Covid = () => {
+
+
+    const { data:dataCovid, loading, isError } = useFetch('https://api.covidtracking.com/v1/us/daily.json')
+
     function parseDate(dateString) {
         // Kiểm tra độ dài chuỗi phải là 8 và là số
         if (dateString.length !== 8 || isNaN(dateString)) {
-          throw new Error('Chuỗi ngày không hợp lệ');
+            throw new Error('Chuỗi ngày không hợp lệ');
         }
-      
+
         // Chia chuỗi thành các thành phần ngày, tháng và năm
         const year = dateString.slice(0, 4);
         const month = dateString.slice(4, 6);
         const day = dateString.slice(6, 8);
-      
+
         // Tạo đối tượng Date từ các thành phần đã chia
         const formattedDate = `${day}-${month}-${year}`;
-  
+
         return formattedDate;
-      }
+    }
 
-    const [dataCovid, setDataCovid] = useState([])
-    useEffect(() => {
-        async function getData() {
-            try {
-                // You can await here
-                let res = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
-                // Xử lý dữ liệu hoặc set state tại đây
-                return res;
-                
-            } catch (error) {
-                // Xử lý lỗi nếu có
-            }
+    // const [dataCovid, setDataCovid] = useState([])
+    // const [loading, setLoading] = useState(true)
+    // const [isError, setIsError] = useState(false)
+    console.log(dataCovid)
 
-        }
-        
-        // let data = getData() && getData().data ? getData().data : [];
-        let data = getData();
-        data.then((data) => {
-            console.log('Dữ liệu từ server:', data); // Đây là dữ liệu thực sự từ Promise
-        console.log(data)
-        console.log(parseDate('20210307'))
-        setDataCovid(data.data)
-
-          }).catch((error) => {
-            console.error('Đã xảy ra lỗi:', error);
-          });
-    }, [])
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Death</th>
-                    <th>Hospitalized</th>
-                </tr>
-            </thead>
-            <tbody>
-                {dataCovid && dataCovid.length > 0 && dataCovid.map(item => {
-                    if(item.hospitalized !== null){
+        <>
+            <h3>Covid 19 Data</h3>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Death</th>
+                        <th>Hospitalized</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {isError === false && loading === false && dataCovid && dataCovid.length > 0 && dataCovid.map(item => {
+
+                       if(item.hospitalized !== null){
+
                         return (
                             <tr key={item.hash}>
                                 <td>{parseDate(String(item.date))}</td>
@@ -65,12 +52,33 @@ const Covid = () => {
                                 <td>{item.hospitalized}</td>
                             </tr>
                         )
-                    }
-                })}
+                       }
+                    //    else{
+                    //     return (
+                    //         <tr key={item.hash}>
+                    //             <td>haha</td>
+                    //             <td>ok</td>
+                    //             <td>er</td>
+                    //         </tr>
+                    //     )
+                    //    }
 
-               
-            </tbody>
-        </table>
+                    })}
+                    {loading === true &&
+                        <tr>
+                            <td colSpan='5' style={{ 'textAlign': 'center' }}>Loading....</td>
+                        </tr>
+                    }
+                    {isError === true &&
+                        <tr>
+                            <td colSpan='5' style={{ 'textAlign': 'center' }}>Something wrong...</td>
+                        </tr>
+                    }
+
+
+                </tbody>
+            </table>
+        </>
     )
 }
 export default Covid;
